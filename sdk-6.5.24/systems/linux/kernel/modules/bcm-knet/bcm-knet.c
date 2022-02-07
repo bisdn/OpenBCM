64a6f5be276807606223e6b0cdfa9af692bfe9a4
Jonas Gorski <jonas.gorski@bisdn.de>
2022-02-07 12:11:54 +0100

    bcm-knet: report link state
    We set netif carrier, so we can just use ethtool_op_get_link.
    
    Signed-off-by: Jonas Gorski <jonas.gorski@bisdn.de>

--- sdk-6.5.24/systems/linux/kernel/modules/bcm-knet/bcm-knet.c
+++ sdk-6.5.24/systems/linux/kernel/modules/bcm-knet/bcm-knet.c
@@ -6737,6 +6737,7 @@ bkn_get_ts_info(struct net_device *dev, struct ethtool_ts_info *info)
 
 static const struct ethtool_ops bkn_ethtool_ops = {
     .get_drvinfo        = bkn_get_drvinfo,
+    .get_link           = ethtool_op_get_link,
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,5,0))
     .get_ts_info        = bkn_get_ts_info,
 #endif
